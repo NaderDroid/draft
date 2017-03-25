a9db7c23eede8cb66b48776d6299c88765796981
NaderDroid <althubaity@hotmail.com>
2017-03-25 18:24:00 -0400

    Initial commit

---
+++ _js/timer.js
@@ -0,0 +1,34 @@
+/**
+ * Created by na511dr on 3/24/17.
+ */
+
+var end = new Date('04/04/2017 6:09 PM');
+
+var _second = 1000;
+var _minute = _second * 60;
+var _hour = _minute * 60;
+var _day = _hour * 24;
+var timer;
+
+function showRemaining() {
+    var now = new Date();
+    var distance = end - now;
+    if (distance < 0) {
+
+        clearInterval(timer);
+        document.getElementById('countdown').innerHTML = 'EXPIRED!';
+
+        return;
+    }
+    var days = Math.floor(distance / _day);
+    var hours = Math.floor((distance % _day) / _hour);
+    var minutes = Math.floor((distance % _hour) / _minute);
+    var seconds = Math.floor((distance % _minute) / _second);
+
+    document.getElementById('countdown').innerHTML = days + 'days ';
+    document.getElementById('countdown').innerHTML += hours + 'hrs ';
+    document.getElementById('countdown').innerHTML += minutes + 'mins ';
+    document.getElementById('countdown').innerHTML += seconds + 'secs';
+}
+
+timer = setInterval(showRemaining, 1000);
